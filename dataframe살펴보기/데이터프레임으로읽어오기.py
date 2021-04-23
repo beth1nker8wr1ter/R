77fd968060636b4bee5ca148916522f010c8a2a0
tjlee314 <tjlee314@hanmail.net>
2021-04-23 10:39:54 +0900

    왜안된다는겨!

---
+++ dataframe살펴보기/데이터프레임으로읽어오기.py
@@ -0,0 +1,15 @@
+
+# 데이터를 2차원 데이터의 형태인 테이블로 읽어오려면 pandas 라이브러리 필요!
+
+import pandas as pd
+import numpy as np
+#import seaborn as sb
+
+
+df = pd.read_csv("../datas/2010.csv", encoding='utf8')
+print(df)
+
+
+print(7/5)
+
+
